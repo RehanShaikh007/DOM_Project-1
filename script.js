2ec50f1ee3c188c8eb9d53c2fd6553461f07eded
RehanShaikh007 <rehanmukhtarshaikh7@gmail.com>
2024-02-28 10:42:32 +0530

    Added

---
+++ script.js
@@ -0,0 +1,9 @@
+let jsdiv = document.getElementById("head");
+jsdiv.style.backgroundColor = "yellow";
+jsdiv.style.margin = "20px";
+jsdiv.style.padding = "10px";
+jsdiv.style.fontSize = "18px"
+jsdiv.style.fontWeight = "bold";
+jsdiv.style.height = "200px";
+jsdiv.style.width = "300px";
+jsdiv.style.color = "blue";
